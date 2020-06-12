0c9e1821110ed7435bb69967e88b83937daf7669
ak4550 <36280722+ak4550@users.noreply.github.com>
2020-06-12 17:18:38 +0530

    android

--- app/src/main/java/co/ak/notes/Adapter.java
+++ app/src/main/java/co/ak/notes/Adapter.java
@@ -1,11 +1,14 @@
 package co.ak.notes;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.net.ProtocolFamily;
@@ -14,10 +17,12 @@ import java.util.List;
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder>{
 
     private List<Note> mNotes;
+    Context context;
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.recyclerview_item, parent, false);
 
@@ -34,6 +39,12 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder>{
         holder.txtPriority.setText(String.valueOf(note.getPriority()));
         holder.txtTitle.setText(note.getTitle());
         holder.txtDescription.setText(note.getDescription());
+        holder.container.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "Touched", Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
     }
@@ -51,6 +62,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder>{
 
     public class ViewHolder extends RecyclerView.ViewHolder{
         TextView txtTitle, txtDescription, txtPriority;
+        CardView container;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -58,6 +70,8 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder>{
             txtTitle = itemView.findViewById(R.id.txtTitle);
             txtDescription = itemView.findViewById(R.id.txtDescription);
             txtPriority = itemView.findViewById(R.id.txtPriority);
+            container = itemView.findViewById(R.id.container);
+
         }
     }
 }
